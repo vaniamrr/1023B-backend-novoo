7a91d3dbbe071c052050828d9bc9a248628d10fa
vaniamrr <vania.santos3@estudante.ifms.edu.br>
2025-08-27 11:29:44 -0400

    Finalização do código de conexão do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,50 @@
+import express from 'express'
+import mysql from 'mysql2/promise'
 import 'dotenv/config'
-console.log(process.env.DBHOST);
+
+const app = express()
+app.get('/', async (req, res) => {
+      if (process.env.DBHOST === undefined){
+            res.status(500).send('DBHOST não está definido nas variáveis de ambiente!')
+            return
+        }
+        if (process.env.DBUSER === undefined){
+            res.status(500).send('DBUSER não está definido nas variáveis de ambiente!') 
+            return
+        }
+        if (process.env.DBPASSWORD === undefined){
+            res.status(500).send('DBPASSWORD não está definido nas variáveis de ambiente!') 
+            return
+        }
+        if (process.env.DBNAME === undefined){
+            res.status(500).send('DBNAME não está definido nas variáveis de ambiente!') 
+            return
+        }
+        if (process.env.DBPORT === undefined){
+            res.status(500).send('DBPORT não está definido nas variáveis de ambiente!') 
+            return
+        }
+        
+        try{
+        const conn = await mysql.createConnection({
+        host: process.env.DBHOST,
+        user: process.env.DBUSER,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBNAME,
+        port: Number(process.env.DBPORT)
+        })
+        res.send('Conectado ao banco de dados com sucesso!') 
+        }
+
+    catch (err) {
+        if(err instanceof Error === false){
+            res.status(500).send('Erro desconhecido ao conectar ao banco de dados') 
+        }
+        const error = err as Error
+        res.status(500).send('Erro ao conectar ao banco de dados: ' + error.message)
+}
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000')
+})
